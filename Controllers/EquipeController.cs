d8e7e2b71093d8705131329bf07a7942454f8a88
unknown <rafacs1404@gmail.com>
2020-07-15 10:09:17 -0300

    Upload de imagens e método Excluir desenvolvidos para Equipes

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -28,14 +29,42 @@ namespace Eplayers.Controllers
             Equipe novaEquipe = new Equipe();
             novaEquipe.IdEquipe = Int32.Parse(form["IdEquipe"]);
             novaEquipe.Nome = form["Nome"];
-            novaEquipe.Imagem = form["Imagem"];
 
-            equipeModel.Create(novaEquipe);
+             // Upload Início
+            var file = form.Files[0];
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+            if(file != null)
+            {
+                if(!Directory.Exists(folder)){
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))  
+                {  
+                    file.CopyTo(stream);  
+                }
+                novaEquipe.Imagem   = file.FileName;
+            }
+            else
+            {
+                novaEquipe.Imagem   = "padrao.png";
+            }
+            // Upload Final
+            
+            equipeModel.Create(novaEquipe);         
 
             ViewBag.Equipes = equipeModel.ReadAll();
 
             return LocalRedirect("~/Equipe");
-        }   
+        }
+        
+        [Route("{id}")]
+        public IActionResult Excluir(int id){
+            equipeModel.Delete(id);
+            return LocalRedirect("~/Equipe");
+        }
 
     }
 }
